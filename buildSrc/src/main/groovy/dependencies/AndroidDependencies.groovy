9989ad82166e66c44dd565019f1002995e8f0d28
Antonio Bertucci <archano@gmail.com>
2018-09-06 23:25:36 +0200

    Add missing Android AppCompat dependency

--- buildSrc/src/main/groovy/dependencies/AndroidDependencies.groovy
+++ buildSrc/src/main/groovy/dependencies/AndroidDependencies.groovy
@@ -3,4 +3,5 @@ package dependencies
 final class AndroidDependencies {
 
     final String plugin = 'com.android.tools.build:gradle:3.1.4'
+    final String appCompat = 'com.android.support:appcompat-v7:27.1.1'
 }
