5ac1deeeb988efc65f33de4787575ebbf58dba2a
Antonio Bertucci <archano@gmail.com>
2018-09-08 16:31:19 +0200

    Introduce plugin to apply sharable Android configuration

---
+++ buildSrc/src/main/groovy/AndroidConfigurationPlugin.groovy
@@ -0,0 +1,30 @@
+import org.gradle.api.Plugin
+import org.gradle.api.Project
+
+class AndroidConfigurationPlugin implements Plugin<Project> {
+
+    @Override
+    void apply(Project project) {
+        project.plugins.withId('com.android.application') {
+            addAndroidUtils(project.android, project.rootProject.version)
+        }
+        project.plugins.withId('com.android.library') {
+            addAndroidUtils(project.android, project.rootProject.version)
+        }
+    }
+
+    private static void addAndroidUtils(android, version) {
+        android.ext.applySdk = {
+            delegate = android
+
+            compileSdkVersion 27
+
+            defaultConfig {
+                minSdkVersion 21
+                targetSdkVersion 27
+                versionCode 1
+                versionName version
+            }
+        }
+    }
+}
