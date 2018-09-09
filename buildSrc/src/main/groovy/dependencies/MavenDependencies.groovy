9c307af1a4cd5d8fb46efdfcce4516a70ed907a2
Antonio Bertucci <archano@gmail.com>
2018-09-09 16:36:23 +0200

    Apply gradle-build-properties-plugin to every sub-project

--- buildSrc/src/main/groovy/dependencies/MavenDependencies.groovy
+++ buildSrc/src/main/groovy/dependencies/MavenDependencies.groovy
@@ -3,5 +3,6 @@ package dependencies
 final class MavenDependencies {
 
     final AndroidDependencies android = new AndroidDependencies()
+    final String gradleBuildPropertiesPlugin = 'com.novoda:gradle-build-properties-plugin:0.4.1'
     final KotlinDependencies kotlin = new KotlinDependencies()
 }
