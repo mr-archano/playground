a4fea44e796e480ca3bafc078f1e24db8136da0d
Antonio Bertucci <archano@gmail.com>
2018-09-10 01:16:58 +0200

    Add Moshi and Retrofit dependencies

--- buildSrc/src/main/groovy/dependencies/MavenDependencies.groovy
+++ buildSrc/src/main/groovy/dependencies/MavenDependencies.groovy
@@ -6,7 +6,9 @@ final class MavenDependencies {
     final String gradleBuildPropertiesPlugin = 'com.novoda:gradle-build-properties-plugin:0.4.1'
     final String junit = 'junit:junit:4.12'
     final KotlinDependencies kotlin = new KotlinDependencies()
+    final String moshi = 'com.squareup.moshi:moshi:1.6.0'
     final String okhttp = 'com.squareup.okhttp3:okhttp:3.11.0'
+    final RetrofitDependencies retrofit = new RetrofitDependencies()
     final String rxjava = 'io.reactivex.rxjava2:rxjava:2.2.0'
     final String truth = 'com.google.truth:truth:0.42'
 }
