43d238ba197a314e4dd6c3ccfffed7c67f02cb7e
Antonio Bertucci <archano@gmail.com>
2018-09-09 15:47:44 +0200

    Introduce custom plugin to generate Jacoco reports for Android unit tests

--- buildSrc/src/main/groovy/PlaygroundPlugin.groovy
+++ buildSrc/src/main/groovy/PlaygroundPlugin.groovy
@@ -1,11 +1,13 @@
 import org.gradle.api.Plugin
 import org.gradle.api.Project
 import plugins.AndroidConfigurationPlugin
+import plugins.AndroidJacocoReportPlugin
 
 class PlaygroundPlugin implements Plugin<Project> {
 
     @Override
     void apply(Project project) {
         new AndroidConfigurationPlugin().apply(project)
+        new AndroidJacocoReportPlugin().apply(project)
     }
 }
