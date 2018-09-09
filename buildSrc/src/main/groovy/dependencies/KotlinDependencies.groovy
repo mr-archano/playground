21a7321366b93fc4cf459cf0fde9a61763c16e5e
Antonio Bertucci <archano@gmail.com>
2018-09-09 15:38:34 +0200

    Apply Kotlin plugin to app module

---
+++ buildSrc/src/main/groovy/dependencies/KotlinDependencies.groovy
@@ -0,0 +1,13 @@
+package dependencies
+
+final class KotlinDependencies {
+
+    private static final String VERSION = '1.2.61'
+
+    final String plugin = module('gradle-plugin')
+    final String stdlib = module('stdlib')
+
+    private static String module(String name, String version = VERSION) {
+        return "org.jetbrains.kotlin:kotlin-$name:$version"
+    }
+}
--- buildSrc/src/main/groovy/dependencies/MavenDependencies.groovy
+++ buildSrc/src/main/groovy/dependencies/MavenDependencies.groovy
@@ -3,4 +3,5 @@ package dependencies
 final class MavenDependencies {
 
     final AndroidDependencies android = new AndroidDependencies()
+    final KotlinDependencies kotlin = new KotlinDependencies()
 }
