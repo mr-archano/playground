e8698b3b32702e8df91b7e15cc02111ab220dc67
Antonio Bertucci <archano@gmail.com>
2018-09-23 13:50:41 +0200

    Extract constant for base url

--- features/letterboxd/api/src/main/kotlin/com/letterboxd/authentication/RetrofitAccessTokenFetcher.kt
+++ features/letterboxd/api/src/main/kotlin/com/letterboxd/authentication/RetrofitAccessTokenFetcher.kt
@@ -13,6 +13,8 @@ import retrofit2.http.FormUrlEncoded
 import retrofit2.http.Headers
 import retrofit2.http.POST
 
+private const val LETTERBOX_BASE_URL = "https://api.letterboxd.com/api/v0/"
+
 internal class RetrofitAccessTokenFetcher private constructor(private val backend: AccessTokenBackend) : AccessTokenFetcher {
 
     private val convertJson: Function<AccessTokenJson, AccessToken> = Function { json: AccessTokenJson ->
@@ -29,7 +31,7 @@ internal class RetrofitAccessTokenFetcher private constructor(private val backen
                     .addNetworkInterceptor(signingRequestDecorator.asInterceptor())
                     .build()
             val backend = Retrofit.Builder()
-                    .baseUrl("https://api.letterboxd.com/api/v0/")
+                    .baseUrl(LETTERBOX_BASE_URL)
                     .client(okHttpClient)
                     .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
                     .addConverterFactory(MoshiConverterFactory.create(Moshi.Builder().build()))
@@ -60,6 +62,7 @@ private interface AccessTokenBackend {
     fun createAccessToken(@Field("username") username: String,
                           @Field("password") password: String,
                           @Field("grant_type") grantType: String = "password"): Single<AccessTokenJson>
+
     @Headers("Accept: application/json")
     @FormUrlEncoded
     @POST("auth/token")
